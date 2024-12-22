1cf2ea1a389f29c90e01a7851ff59ab9be4432e9
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-22 18:36:42 +0100

    chore: improved `postinstall` scripts

--- scripts/postinstall/index.js
+++ scripts/postinstall/index.js
@@ -1,12 +1,14 @@
-const tsSetup = require( './ts-setup.js' )
+const Chain		= require( '@alessiofrittoli/chain-functions' )
+const tsSetup	= require( './ts-setup.js' )
 
-/** @type {(() => void | Promise<void>)[]} */
-const scripts = [ tsSetup ]
-
-const executePostInstall = async () => {
-	await Promise.all( scripts.map( async script => {
-		await script()
-	} ) )
+/** @type {import( '@alessiofrittoli/chain-functions/types' ).LastChainLink<() => void | Promise<void>>} */
+const lastChainLink = () => () => {
+	console.log( 'Post-install done.' )
 }
 
-executePostInstall()
+/** @type {import( '@alessiofrittoli/chain-functions/types' ).ChainFactory<() => void | Promise<void>>} */
+const chain = [ tsSetup, lastChainLink ]
+
+;( async () => {
+	await Chain.functions( chain )()
+} )()
--- scripts/postinstall/ts-setup.js
+++ scripts/postinstall/ts-setup.js
@@ -1,39 +1,17 @@
-const fs		= require( 'fs' )
-const path		= require( 'path' )
-const project	= require( '../../package.json' )
+const project = require( '../../package.json' )
+const { addTypesReference } = require( '@alessiofrittoli/post-install-scripts' )
 
 
-const tsSetup = () => {
-
-	const projectName	= project.name.replace( '@alessiofrittoli/', '' )
-	const projectRoot	= process.env.INIT_CWD || process.cwd()
-	const typesFilename	= `${ projectName }.d.ts`
-
-	if ( projectRoot.endsWith( projectName ) ) {
-		console.log( `Skip "postinstall" script. Running in ${ project.name }` )
-		return
+/** @type {import( '@alessiofrittoli/chain-functions/types' ).ChainLink<() => void | Promise<void>>} */
+const tsSetup = next => () => {
+	
+	/** @type {import( '@alessiofrittoli/post-install-scripts/types' ).AddTypesReferenceOptions} */
+	const options = {
+		projectName: project.name,
 	}
-	if ( ! projectRoot ) {
-		console.error( 'INIT_CWD is not set. This script must be run during pnpm install.' )
-		process.exit( 1 )
-	}
+	addTypesReference( options )
 
-	const tsconfigPath = path.resolve( projectRoot, typesFilename )
-
-	const data = [
-		`/// <reference types="${ project.name }" />`,
-		'// NOTE: This file should not be edited'
-	].join( '\n\n' )
-
-	try {
-		fs.writeFileSync( tsconfigPath, data )
-		console.log( `"${ typesFilename }" has been created at the root of your project.` )
-		console.log( 'Please update your tsconfig.json to add this file in your "include" property of your tsconfig.json file.' )
-	} catch ( error ) {
-		console.error( `An error occured while creating "${ typesFilename }" at the root of your project. Some global types may not working as expect.`, error )
-		process.exit( 1 )
-	}
-
+	return next()
 }
 
 module.exports = tsSetup
