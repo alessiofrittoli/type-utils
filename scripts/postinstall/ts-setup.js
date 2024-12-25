a7785f60fda993ec0303aa96541cdc5010d40fcf
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2024-12-25 01:45:51 +0100

    chore: migrate `@alessiofrittoli/post-install-scripts` to `@alessiofrittoli/node-scripts` (#23)

--- scripts/postinstall/ts-setup.js
+++ scripts/postinstall/ts-setup.js
@@ -1,15 +1,12 @@
+const { addTypesReference } = require( '@alessiofrittoli/node-scripts/postinstall' )
 const project = require( '../../package.json' )
-const { addTypesReference } = require( '@alessiofrittoli/post-install-scripts' )
-
 
 /** @type {import( '@alessiofrittoli/chain-functions/types' ).ChainLink<() => void | Promise<void>>} */
 const tsSetup = next => () => {
 	
-	/** @type {import( '@alessiofrittoli/post-install-scripts/types' ).AddTypesReferenceOptions} */
-	const options = {
-		projectName: project.name,
-	}
-	addTypesReference( options )
+	addTypesReference( {
+		name: project.name,
+	} )
 
 	return next()
 }
