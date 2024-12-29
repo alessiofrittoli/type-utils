149b100ce78d6d465a6aed87d4789d7f680d2a46
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-29 20:10:49 +0100

    chore: minor improvements

--- scripts/postinstall/index.js
+++ scripts/postinstall/index.js
@@ -1,14 +1,16 @@
 const { Chain }	= require( '@alessiofrittoli/chain-functions' )
 const tsSetup	= require( './ts-setup.js' )
+const project = require( '../../package.json' )
 
 /** @type {import( '@alessiofrittoli/chain-functions/types' ).LastChainLink<() => void | Promise<void>>} */
 const lastChainLink = () => () => {
-	console.log( 'Post-install done.' )
+	console.log( {
+		package: project.name,
+		message: 'Post-Install done'
+	  } )
 }
 
 /** @type {import( '@alessiofrittoli/chain-functions/types' ).ChainFactory<() => void | Promise<void>>} */
 const chain = [ tsSetup, lastChainLink ]
 
-;( async () => {
-	await Chain.functions( chain )()
-} )()
+Chain.functions( chain )()
